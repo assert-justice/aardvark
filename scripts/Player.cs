ab5583be15a7621b132a154c1775d9d372f3d0f5
Riley <waskerdu@gmail.com>
2025-06-22 16:48:13 -0700

    fuck godot

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,49 +3,40 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-    [Export] float Speed = 300;
-    [Export] float DragSpeed = 100;
-    Node2D arm;
-    RayCast2D ray;
-    RigidBody2D held = null;
-    public override void _Ready()
-    {
-        base._Ready();
-        arm = GetNode<Node2D>("Arm");
-        ray = GetNode<RayCast2D>("Arm/RayCast2D");
-    }
+	[Export] float Speed = 300;
+	Node2D arm;
+	RayCast2D ray;
+	public override void _Ready()
+	{
+		base._Ready();
+		arm = GetNode<Node2D>("Arm");
+		ray = GetNode<RayCast2D>("Arm/RayCast2D");
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        var move = Input.GetVector("left", "right", "up", "down");
-        if (held == null)
-        {
-            if (move.Length() > 0 && (move.X == 0 || move.Y == 0))
-            {
-                float aim = move.Angle();
-                arm.Rotation = aim;
-            }
-            Velocity = move * Speed;
-            if (Input.IsActionPressed("grab") && ray.IsColliding())
-            {
-                if (ray.GetCollider() is RigidBody2D rb)
-                {
-                    held = rb;
-                    held.LinearDamp = 0;
-                }
-            }
-        }
-        else
-        {
-            Velocity = move * DragSpeed;
-            held.LinearVelocity = Velocity;
-            if (!Input.IsActionPressed("grab") || !ray.IsColliding())
-            {
-                held.LinearVelocity *= 0;
-                held.LinearDamp = 10;
-                held = null;
-            }
-        }
-        MoveAndSlide();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+		// float dt = (float)delta;
+		var velocity = Velocity;
+		var move = Input.GetVector("left", "right", "up", "down");
+		if (move.Length() > 0 && (move.X == 0 || move.Y == 0))
+		{
+			float aim = move.Angle();
+			arm.Rotation = aim;
+		}
+		velocity = move * Speed;
+		Velocity = velocity;
+		// arm.Visible = false;
+		if (ray.IsColliding())
+		{
+			// arm.Visible = true;
+			// GD.Print(ray.GetCollider());
+			var target = ray.GetCollider();
+			if (target is RigidBody2D rb && Input.IsActionPressed("grab"))
+			{
+				rb.LinearVelocity = velocity;
+			}
+		}
+		MoveAndSlide();
+	}
 }
